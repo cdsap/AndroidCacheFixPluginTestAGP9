f45887fd1236b3ad235f261b0aaf2546cba9c278
Inaki Villar <inaki.seri@gmail.com>
2025-09-29 09:29:05 -0700

    sl

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,6 +4,9 @@ pluginManagement {
         google()
         mavenCentral()
         gradlePluginPortal()
+         maven {
+           url = uri("https://plugins.grdev.net/m2")
+        }
     }
 }
 plugins {
