5b181d9564dacd943c3519bbbc0bc02b6cb3eba7
Inaki Villar <inaki.seri@gmail.com>
2025-09-29 09:19:51 -0700

    3_0_1 jdk21

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,5 +7,11 @@ plugins {
     alias(libs.plugins.kotlin.ksp) apply false
     alias(libs.plugins.hilt) apply false
     id("com.autonomousapps.dependency-analysis") version "2.19.0" apply true
+    id("org.gradle.android.cache-fix") version "3.0.1" apply false
+}
 
+subprojects {
+   plugins.withType<com.android.build.gradle.api.AndroidBasePlugin>() {
+       apply(plugin = "org.gradle.android.cache-fix")
+   }
 }
